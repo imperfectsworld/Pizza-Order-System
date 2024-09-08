9a725fb4f8b44f9a15728b14db72b32bcbc6f358
imperfectsworld <imperfectsworld@gmail.com>
2024-09-08 19:55:26 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'GrandChirpusLab';
+
+
+menu: MenuItem[] = [
+  {name: "Chicken Fingers", category: "Dinner" , price: 11.99, vegetarian: false, soldOut: false},
+  {name: "Pizza", category: "Dinner" , price: 11.99, vegetarian: true, soldOut: false},
+  {name: "Pepperoni Pizza", category: "Dinner" , price: 12.99, vegetarian: false, soldOut: false},
+  {name: "Bread Sticks", category: "Sides" , price: 4.99, vegetarian: true, soldOut: true},
+  {name: "Wings", category: "Sides" , price: 8.99, vegetarian: false, soldOut: false},
+  {name: "Cinnamon Roll", category: "Dessert" , price: 8.99, vegetarian: true, soldOut: false}
+];
+
+/*
+When that "Add to order" button is clicked, call a method that takes 
+the price of the selected item and adds it to the total. Display the total on the page.
+*/
+
+total: number = 0;
+
+addToOrder(price:number)
+{
+  this.total += price;
+}
+
+
 }
